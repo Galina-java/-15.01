88d55c6f7df6c98d37ed0a9cf4b5e556e8bb85b7
Gala <gala.skillfactory@gmail.com>
2024-01-18 06:35:41 +0300

    .

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 3, 5, 6, 7, 8 };
+        int temp;
+        Console.Write("Исходный массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+        Console.Write("\nПеревернутый массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
